835077e802b45408c09a26021099c236fe6f7eb8
anphamvan <anphamvan@gmail.com>
2025-06-05 11:06:33 +0700

    Bo sung Junit test

--- src/test/java/com/project/demo/DemoApplicationTests.java
+++ src/test/java/com/project/demo/DemoApplicationTests.java
@@ -1,13 +1,43 @@
 package com.project.demo;
 
+import com.project.demo.entity.Event;
+import com.project.demo.framework.TenantContext;
+import com.project.demo.repository.EventRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestExecutionListeners;
+import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
+
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Random;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+//@TestExecutionListeners(listeners = { DependencyInjectionTestExecutionListener.class })
 class DemoApplicationTests {
+	@Autowired
+	EventRepository eventRepository;
 
 	@Test
 	void contextLoads() {
 	}
 
+	@Test
+	void testSaveSchema() {
+		Event event = new Event();
+		event.setTimestamp(new Timestamp((new Date()).getTime()));
+		event.setEventNumber(new Random().nextInt(255));
+
+		TenantContext.setTenant("public");
+		var test = eventRepository.saveAndFlush(event);
+		assert(test.getEventNumber() == event.getEventNumber());
+
+
+		TenantContext.setTenant("test");
+		var test2 = eventRepository.saveAndFlush(event);
+		assert(test.getEventNumber() == event.getEventNumber());
+	}
 }
